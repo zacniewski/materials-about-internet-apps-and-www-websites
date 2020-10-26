9fe1cf02e7e7da506612fa609ace9bd33903be46
Artur Zacniewski <a.zacniewski@gmail.com>
2020-10-26 21:12:14 +0100

    addEventListener explained

--- 185-java-script/scripts/3-event-handling.js
+++ 185-java-script/scripts/3-event-handling.js
@@ -11,7 +11,7 @@ zdarzeń bezpośrednio w kodzie HTML:
 */
 
 function buttonClickHandler() {
-alert('Kliknięto w przycisk!');
+console.log('Kliknięto w przycisk o id=guzik!');
 }
 
 /*
@@ -26,5 +26,39 @@ addEventListener:
 
 const btn = document.getElementById('btn');
 btn.addEventListener('click', () => {
-    alert('Kliknięto w przycisk!');
+    console.log('Kliknięto w przycisk - addEventListener nr 1!');
 });
+
+/*
+Każda referencja do elementu DOM posiada m.in. metodę addEventListener, służącą do rejestrowania
+obsługi różnych zdarzeń.
+Metoda ta przyjmuje dwa lub trzy parametry (trzeci, domyślnie o wartości false).
+
+Najpierw musimy określić, na jakie zdarzenie chcemy nasłuchiwać. 
+W naszym przypadku jest to kliknięcie przycisku, czyli zdarzenie
+click. Zauważ jednocześnie, że tym razem nie stosujemy dodatkowego przedrostka on.
+
+Drugim parametrem metody addEventListener jest funkcja, która zostanie wywołana po wykry-
+ciu danego zdarzenia na wskazanym elemencie. Zastosowaliśmy tutaj zapis wykorzystujący
+tzw. funkcje strzałkowe (arrow function). Nie musimy naszej funkcji definiować bezpośrednio
+w definicji metody addEventListener; możemy w tym miejscu przekazać referencję do naszej
+funkcji:
+*/
+
+const btn2 = document.getElementById('btn2');
+function clickHandler() {
+    console.log('Kliknięto w przycisk - addEventListener nr 2!');
+}
+btn2.addEventListener('click', clickHandler);
+
+/*
+Bezpośrednie przypisywanie funkcji obsługującej zdarzenie do pola w referen-
+cji do elementu DOM może dawać czasami niespodziewane efekty. 
+Problem ten rozwiązuje metoda addEventListener, która po wywołaniu kilka razy na tej samej referencji z tym samym
+zdarzeniem nie spowoduje nadpisania, lecz dodanie kolejnych funkcji dla obsługi danego
+zdarzenia. Zostaną one wywołane w takiej kolejności, w jakiej zostały zdefiniowane:
+*/
+
+const btn3 = document.getElementById('btn3');
+btn3.addEventListener('click', () => console.log('Kliknięto w przycisk btn3'));
+btn3.addEventListener('click', () => console.log('Druga funkcja dla zdarzenia click dla btn3'));
