966df41616a3a921809d5cd701e1c2dfb53676a8
Artur Zacniewski <a.zacniewski@gmail.com>
2020-10-26 13:34:42 +0100

    Klasy z konstruktorem __init()__

---
+++ python/1-klasy.py
@@ -0,0 +1,76 @@
+class Dog:
+    def __init__(self, name, age):  # 'name' i 'age' to parametry
+        self.name = name  # atrybut 'name'(po lewej) ma przypisaną wartość 'parametru 'name' (tę z prawej)
+        self.age = age
+
+# utworzenie obiektu klasy Dog        
+dog1 = Dog("Burek", 4)
+print(f"{dog1.name} ma {dog1.age} lata")
+
+"""
+__init__ to konstruktor, czyli ustawia początkowy stan obiektu 
+poprzez przypisanie wartości właściwości obiektu. 
+Oznacza to, że .__ init __ () inicjuje każdą nową instancję klasy.
+
+Atrybuty utworzone w .__ init __ () nazywane są atrybutami instancji. 
+Wartość atrybutu instancji jest specyficzna dla konkretnego wystąpienia klasy. 
+Wszystkie obiekty Dog mają nazwę i wiek, 
+ale wartości atrybutów 'name' i 'age' będą się różnić w zależności od obiektu (instancji) klasy Dog.
+"""
+
+"""
+Z drugiej strony atrybuty klasy to atrybuty, które mają tę samą wartość dla wszystkich instancji klas. 
+Możesz zdefiniować atrybut klasy, przypisując wartość do nazwy zmiennej poza .__ init __ ().
+Na przykład następująca klasa Dog ma atrybut klasy o nazwie gatunki z wartością „Canis familiaris:
+"""
+
+class Dog2: 
+    # atrybut klasy
+    species = "Canis familiaris"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+# utworzenie obiektu klasy Dog2        
+dog2 = Dog2("Reks", 3)
+print(f"{dog2.name} ma {dog2.age} lata i należy do gatunku {dog2.species}")
+
+"""
+Jedną z największych zalet używania klas do organizowania danych jest to, 
+że instancje mają zagwarantowane atrybuty, których oczekujesz. 
+Wszystkie instancje klasy Dog mają atrybuty .species, .name i .age, 
+więc możesz używać tych atrybutów z pewnością, wiedząc, że zawsze zwrócą wartość.
+
+Chociaż atrybuty na pewno istnieją, ich wartości można zmieniać dynamicznie.
+Co w innych językach programowania jest do osiągnięcia za pomocą setterów/getterów.
+"""
+dog2.age = 5
+print(f"{dog2.name} ma {dog2.age} lata i należy do gatunku {dog2.species}")
+
+"""
+Metody instancji to funkcje zdefiniowane wewnątrz klasy i mogą być wywoływane tylko z instancji tej klasy. 
+Podobnie jak .__ init __ (), pierwszym parametrem metody instancji jest zawsze self.
+"""
+
+class Dog3:
+    species = "Canis familiaris"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # Metoda instancji
+    def description(self):
+        return f"{self.name} ma {self.age} lata"
+
+    # Metoda instancji
+    def speak(self, sound):
+        return f"{self.name} mówi {sound}"
+
+
+# utworzenie obiektu klasy Dog3        
+dog3 = Dog3("Reksio", 2)
+print(dog3.description())
+print(dog3.speak("Hau, hau"))
