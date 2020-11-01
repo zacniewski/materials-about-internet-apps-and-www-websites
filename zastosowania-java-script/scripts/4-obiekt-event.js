a87a13474974a0a8156567682a5af27f1fc82e1a
Artur Zacniewski <a.zacniewski@gmail.com>
2020-11-01 21:36:56 +0100

    Obiekt event

--- zastosowania-java-script/scripts/4-obiekt-event.js
+++ zastosowania-java-script/scripts/4-obiekt-event.js
@@ -33,7 +33,56 @@ zmieniać również inne jego parametry, np. zmieńmy po kliknięciu tło przyci
 szarego na kolor czerwony:
 */
 
-const btn = document.getElementById('btn');
-btn.addEventListener('click', event => {
+const btn2 = document.getElementById('btn2');
+btn2.addEventListener('click', event => {
     event.target.style.backgroundColor = 'red';
 });
+
+/*
+Załóżmy, że chcemy wyświetlić
+na ekranie czerwony prostokąt i analizować położenie myszki po kliknięciu go:
+*/
+
+const box = document.getElementById('box');
+box.addEventListener('click', e => {
+    console.log(`Pozycja myszki: ${e.clientX}, ${e.clientY}`);
+});
+
+/*
+Delegowanie zdarzeń
+W niektórych sytuacjach będziemy chcieli analizować zdarzenia dla wielu elementów zawartych
+w jakimś elemencie nadrzędnym. Załóżmy na przykład, że mamy listę elementów <li>
+i chcemy nasłuchiwać na kliknięcie każdego z nich, aby pobrać jego wartość textContent
+i wstawić w miejsce elementu <span>:
+*/
+
+/*
+Zadanie to moglibyśmy wykonać na kilka sposobów. Możemy np. pobrać kolekcję referencji do
+wszystkich elementów <li> zawartych w elemencie o identyfikatorze list i przypisać do nich
+funkcję obsługującą zdarzenie click. W tym celu możemy wykorzystać metodę forEach,
+pozwalającą na iterację po całej kolekcji, a dla każdego elementu musielibyśmy wywołać
+metodę addEventListener.
+
+Istnieje jednak inny sposób, określany często jako delegacja zdarzeń. Polega to na zarejestrowa-
+niu obsługi danego zdarzenia dla rodzica elementów, które nas interesują, i analizowaniu
+rodzaju elementu, na którym to zdarzenie wystąpiło:
+*/
+
+const selected = document.getElementById('selected');
+const list = document.getElementById('list');
+list.addEventListener('click', e => {
+    const el = e.target;
+    selected.textContent = el.nodeName === 'LI' ? el.textContent : '';
+});
+
+/*
+W tym przykładzie metodę addEventListener wywołaliśmy dla elementu <ul>, a następnie
+sprawdzaliśmy, czy kliknięty element znajdujący się wewnątrz tej listy jest elementem <li>.
+W ten sposób uniknęliśmy potrzeby iterowania po wszystkich elementach listy i rejestro-
+wania obsługi zdarzenia dla każdego z nich oddzielnie. Ponadto takie rozwiązanie pozwala
+nam obsługiwać zdarzenie click również dla elementów listy, które mogłyby zostać stworzone
+dynamicznie w czasie działania aplikacji.
+*/
+
+
+
