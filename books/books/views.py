ec1e5f29fd3c20170514a54b891320643945b8aa
Artur Zacniewski <a.zacniewski@gmail.com>
2020-11-13 10:36:38 +0100

    Search with Q objects

--- books/books/views.py
+++ books/books/views.py
@@ -1,6 +1,7 @@
+from django.db.models import Q
 from django.views.generic import ListView, DetailView # new
-from .models import Book
 
+from .models import Book
 
 class BookListView(ListView):
     model = Book
@@ -12,3 +13,16 @@ class BookDetailView(DetailView): # new
     model = Book
     context_object_name = 'book' # new
     template_name = 'books/book_detail.html'
+
+
+class SearchResultsListView(ListView): # new
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    # queryset = Book.objects.filter(title__icontains='pi')
+
+    def get_queryset(self): # new
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
