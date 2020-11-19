d52b875130e5cebd935672e557add39bea6c2331
Artur Zacniewski <a.zacniewski@gmail.com>
2020-11-19 21:04:26 +0100

    Hiding messages with jQuery

--- social-website/images/views.py
+++ social-website/images/views.py
@@ -37,3 +37,10 @@ def image_create(request):
                   'images/image/create.html',
                   {'section': 'images',
                    'form': form})
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+                  'images/image/detail_v1.html',
+                  {'section': 'images',
+                   'image': image})
