bdaca34a51a73154f7911163deea3e02d522ef47
Artur Zacniewski <a.zacniewski@gmail.com>
2020-11-27 08:43:47 +0100

    Black

--- Python-zastosowania/4-web-scraping-part1.py
+++ Python-zastosowania/4-web-scraping-part1.py
@@ -1,4 +1,7 @@
 import requests
 
+from bs4 import BeautifulSoup
+
 odpowiedz = requests.get("https://pl.wikipedia.org/wiki/Zygmunt_III_Waza")
 print("odpowiedź = \n", odpowiedz)
+print("odpowiedź tekstowa = \n", odpowiedz.text)
