ae6bf060efff6a9e2003db425b07a15918799fb3
Artur Zacniewski <a.zacniewski@gmail.com>
2020-11-29 13:45:20 +0100

    Uproszczona struktura projektu

--- React-pierwsze-kroki/react-pierwsze-kroki/src/index.js
+++ React-pierwsze-kroki/react-pierwsze-kroki/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; // importujemy "główny" plik CSS
-import App from './App'; // import komponentu App z pliku App.js
 import Hello from './Hello' // nasz komponent Hello
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,8 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
