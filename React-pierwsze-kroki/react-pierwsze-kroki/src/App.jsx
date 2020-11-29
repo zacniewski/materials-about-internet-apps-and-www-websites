ec92def18380eaa09c673ae47d6b28390f5bcbb9
Artur Zacniewski <a.zacniewski@gmail.com>
2020-11-29 19:08:31 +0100

    Proptype warning

--- React-pierwsze-kroki/react-pierwsze-kroki/src/App.jsx
+++ React-pierwsze-kroki/react-pierwsze-kroki/src/App.jsx
@@ -5,8 +5,8 @@ import Countdown from './Countdown'
 
 const App = () => (
   <div>
-      <Countdown name ="śniadanie" time="7.00" />
-      <Countdown name ="obiad" time="15.00" />
+      <Countdown name ="śniadanie" time="7:00" />
+      <Countdown name ="obiad" time="15:00" />
   </div>
   // sprawdzić drugiego diva!
   );
--- React-pierwsze-kroki/react-pierwsze-kroki/src/Countdown.jsx
+++ React-pierwsze-kroki/react-pierwsze-kroki/src/Countdown.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
 // 'props' jako argument funkcji
-const Countdown = (props) => (
+const Countdown = props => (
     <div className="countdown">
           <b>{props.name}</b> - {props.time}
     </div>
 );
 
 Countdown.propTypes = {
-    name: PropTypes.string,
+    name: PropTypes.number,
     time: PropTypes.string
   };
 export default Countdown;
