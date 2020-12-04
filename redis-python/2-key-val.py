7fc53508e22e4a845fe6719b2ac5554a7f34d473
Artur Zacniewski <a.zacniewski@gmail.com>
2020-12-04 19:27:58 +0100

    Django + Redis + Celery

--- redis-python/2-key-val.py
+++
@@ -1,9 +0,0 @@
-from redis import Redis
-
-redis_connection = Redis()
-
-key = "some-key"
-value = "some-val"
-
-redis_connection.set(key, value)
-print(redis_connection.get(key))
--- redis-python/3-listy.py
+++
@@ -1,9 +0,0 @@
-from redis import Redis
-
-redis_connection = Redis(db=1, decode_responses=True)
-
-list_key = "example-list"
-
-redis_connection.rpush(list_key, 1, 2, 3, 4, 5)
-
-print(redis_connection.lrange(list_key, 0, -1))
