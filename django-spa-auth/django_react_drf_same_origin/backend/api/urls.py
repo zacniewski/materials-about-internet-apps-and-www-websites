456fb9a072719196551a6eecb2d97f3ae735e071
Artur Zacniewski <a.zacniewski@gmail.com>
2020-12-17 21:51:43 +0100

    django_react_drf_same_origin

---
+++ django-spa-auth/django_react_drf_same_origin/backend/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('csrf/', views.get_csrf, name='api-csrf'),
+    path('login/', views.login_view, name='api-login'),
+    path('logout/', views.logout_view, name='api-logout'),
+    path('session/', views.SessionView.as_view(), name='api-session'),  # new
+    path('whoami/', views.WhoAmIView.as_view(), name='api-whoami'),  # new
+]
