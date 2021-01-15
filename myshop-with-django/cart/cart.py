b78495af2ab2b384d046c99f274ffd31ac3ede60
Artur Zacniewski <a.zacniewski@gmail.com>
2021-01-15 12:11:15 +0100

    Views and urls for cart

--- myshop-with-django/cart/cart.py
+++ myshop-with-django/cart/cart.py
@@ -16,7 +16,7 @@ class Cart(object):
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
-     def __iter__(self):
+    def __iter__(self):
         """
         Iterate over the items in the cart and get the products
         from the database.
@@ -40,7 +40,7 @@ class Cart(object):
         """
         return sum(item['quantity'] for item in self.cart.values())
 
-     def add(self, product, quantity=1, override_quantity=False):
+    def add(self, product, quantity=1, override_quantity=False):
         """
         Add a product to the cart or update its quantity.
         """
