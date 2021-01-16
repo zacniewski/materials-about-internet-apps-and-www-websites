54a5e1ada2746df3680cdc1ac9284d94b410a93e
Artur Zacniewski <a.zacniewski@gmail.com>
2021-01-16 14:31:35 +0100

    Basic WS example with client and server

--- prosto-o-web-socket/server.js
+++ prosto-o-web-socket/server.js
@@ -1,11 +1,61 @@
+/**
+Before running:
+> npm install ws
+Then:
+> node server.js
+> open http://localhost:8080 in the browser
+*/
+
 const http = require('http');
-//importing ws module
-const websocket = require('ws');
+const fs = require('fs');
+const ws = new require('ws');
 
-const server = http.createServer((req, res) => {
-    res.end("I am connected");
-});
-//creating websocket server
-const wss = new websocket.Server({ server });
+const wss = new ws.Server({noServer: true});
 
-server.listen(8000);
+const clients = new Set();
+
+function accept(req, res) {
+
+  if (req.url == '/ws' && req.headers.upgrade &&
+      req.headers.upgrade.toLowerCase() == 'websocket' &&
+      // can be Connection: keep-alive, Upgrade
+      req.headers.connection.match(/\bupgrade\b/i)) {
+    wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
+  } else if (req.url == '/') { // index.html
+    fs.createReadStream('./index.html').pipe(res);
+  } else { // page not found
+    res.writeHead(404);
+    res.end();
+  }
+}
+
+function onSocketConnect(ws) {
+  clients.add(ws);
+  log(`new connection`);
+
+  ws.on('message', function(message) {
+    log(`message received: ${message}`);
+
+    message = message.slice(0, 50); // max message length will be 50
+
+    for(let client of clients) {
+      client.send(message);
+    }
+  });
+
+  ws.on('close', function() {
+    log(`connection closed`);
+    clients.delete(ws);
+  });
+}
+
+let log;
+if (!module.parent) {
+  log = console.log;
+  http.createServer(accept).listen(8080);
+} else {
+  // to embed into javascript.info
+  log = function() {};
+  // log = console.log;
+  exports.accept = accept;
+}
