a8b9305b8f241d2749af65f80f6a7c1825c39af4
Artur Zacniewski <a.zacniewski@gmail.com>
2021-01-18 22:05:36 +0100

    Payment process handling

--- myshop-with-django/orders/views.py
+++ myshop-with-django/orders/views.py
@@ -21,9 +21,10 @@ def order_create(request):
             cart.clear()
             # launch asynchronous task
             order_created.delay(order.id)
-            return render(request,
-                          'orders/order/created.html',
-                          {'order': order})
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect for payment
+            return redirect(reverse('payment:process'))
     else:
         form = OrderCreateForm()
     return render(request,
