2c30ea43ee1245b2ba1e61e0f88867d032cee7f6
Artur Zacniewski <a.zacniewski@gmail.com>
2021-01-21 21:37:09 +0100

    Orders management in admin panel

--- myshop-with-django/orders/views.py
+++ myshop-with-django/orders/views.py
@@ -1,3 +1,4 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from cart.cart import Cart
@@ -30,3 +31,11 @@ def order_create(request):
     return render(request,
                   'orders/order/create.html',
                   {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                  'admin/orders/order/detail.html',
+                  {'order': order})
