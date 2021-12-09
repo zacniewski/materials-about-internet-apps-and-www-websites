c9f10ec759fe013f35893b41f2359b8a51cff172
Artur Zacniewski <a.zacniewski@gmail.com>
2021-12-09 18:26:44 +0100

    Permissions at the project level - IsAuthenticatedOrReadOnly

--- BlogAPI-DRF-Django3/config/settings.py
+++ BlogAPI-DRF-Django3/config/settings.py
@@ -131,5 +131,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': [
-    'rest_framework.permissions.AllowAny', ]
+    'rest_framework.permissions.IsAuthenticatedOrReadOnly', ]
 }
