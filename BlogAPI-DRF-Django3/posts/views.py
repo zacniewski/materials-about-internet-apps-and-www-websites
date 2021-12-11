6c5511df143d7b13d7915c9837a6719a11616eb4
Artur Zacniewski <a.zacniewski@gmail.com>
2021-12-11 19:39:11 +0100

    Viewsets and SimpleRouter

--- BlogAPI-DRF-Django3/posts/views.py
+++ BlogAPI-DRF-Django3/posts/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import get_user_model
+from rest_framework import viewsets
 from rest_framework import generics
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer, UserSerializer
 
-
+"""
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
@@ -22,5 +23,17 @@ class UserList(generics.ListCreateAPIView):
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+"""
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
