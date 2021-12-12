3ad3f07e66b0122e68e48a1aa5781112a836ee7c
Artur Zacniewski <a.zacniewski@gmail.com>
2021-12-12 11:54:25 +0100

    Vue.js in Django template

--- blog-na-heroku/blog/views.py
+++ blog-na-heroku/blog/views.py
@@ -1,15 +1,34 @@
+import json
+import random
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posty': posts})
 
+
+def random_items(request):
+    names = ("bob", "dan", "jack", "lizzy", "susan")
+
+    items = []
+    for i in range(100):
+        items.append({
+            "name": random.choice(names),
+            "age": random.randint(20, 80),
+            "url": "https://example.com",
+        })
+
+    return render(request, 'blog/vue_list.html', {"items_json": json.dumps(items)})
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
